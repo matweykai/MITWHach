8decd032b2472a44f20d74025e245e8deee0fddc
coIorbIind <labinfpin13@gmail.com>
2021-10-23 14:42:13 +0300

    Add new archivator Add new jpeg

--- archivator.py
+++ archivator.py
@@ -8,17 +8,17 @@ import zipfile
 # archive_files(name_address_output_zip, where_files_live)
 
 
-def archive_files(name_address_output_zip, where_files_live):
+def archive_files(target_path, file_name):
+    name_address_output_zip = f'{target_path}/My_Zip.zip'
+    abs_file_name = f'{target_path}/{file_name}'
     fantasy_zip = zipfile.ZipFile(name_address_output_zip, 'w')
     try:
-        for folder, subfolders, files in os.walk(where_files_live):
-
+        for folder, subfolders, files in os.walk(abs_file_name):
             for file in files:
                 fantasy_zip.write(os.path.join(folder, file),
-                                  os.path.relpath(os.path.join(folder, file), where_files_live),
+                                  os.path.relpath(os.path.join(folder, file), target_path),
                                   compress_type=zipfile.ZIP_DEFLATED)
-    except IOError as e:
-        print(u'не удалось открыть файл')
-        print(e)
+    except Exception:
+        raise RuntimeError(f"Error during reading file {file_name}")
     finally:
         fantasy_zip.close()
--- jpeg.py
+++ jpeg.py
@@ -1,13 +1,19 @@
 from pdf2image import convert_from_path
-from os import mkdir
+from os import mkdir, path
 
 
-def pdf_to_jpeg(filename, save):
-    pages = convert_from_path(filename, 70, poppler_path=r'C:\Program Files\poppler-0.68.0\bin')
+def pdf_to_jpeg(target_path, file_name, save):
+    abs_folder = f'{target_path}/{save}'
+    abs_file_name = f'{target_path}/{file_name}'
+    if not path.exists(abs_folder):
+        mkdir(abs_folder)
     try:
-        mkdir(save)
+        pages = convert_from_path(abs_file_name, 70, poppler_path='poppler-0.68.0/bin')
+        for i in range(len(pages)):
+            name = abs_folder + "/out" + str(i) + ".jpg"
+            pages[i].save(name, 'JPEG')
     except Exception as e:
-        print(e)
-    for i in range(len(pages)):
-        name = save + "\out" + str(i) + ".jpg"
-        pages[i].save(name, 'JPEG')
+        raise RuntimeError(f"Error during reading file {file_name}")
+    return save
+
+

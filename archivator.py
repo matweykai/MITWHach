876249d1f0f8352790b8b7414bc8d263cac9ed7b
coIorbIind <labinfpin13@gmail.com>
2021-10-23 15:01:56 +0300

    Add comments to archiver

--- archivator.py
+++ archivator.py
@@ -8,9 +8,15 @@ import zipfile
 # archive_files(name_address_output_zip, where_files_live)
 
 
-def archive_files(target_path, file_name):
+def archive_files(target_path, folder_name):
+    """
+    Function which archives folder
+    :param target_path: name of user folder
+    :param folder_name: folder to archive
+    :return: path to archive
+    """
     name_address_output_zip = f'{target_path}/My_Zip.zip'
-    abs_file_name = f'{target_path}/{file_name}'
+    abs_file_name = f'{target_path}/{folder_name}'
     fantasy_zip = zipfile.ZipFile(name_address_output_zip, 'w')
     try:
         for folder, subfolders, files in os.walk(abs_file_name):
@@ -19,6 +25,7 @@ def archive_files(target_path, file_name):
                                   os.path.relpath(os.path.join(folder, file), target_path),
                                   compress_type=zipfile.ZIP_DEFLATED)
     except Exception:
-        raise RuntimeError(f"Error during reading file {file_name}")
+        raise RuntimeError(f"Error during reading file {folder_name}")
     finally:
         fantasy_zip.close()
+    return name_address_output_zip

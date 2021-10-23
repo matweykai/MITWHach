1a6871fade488cc69e7d30de22145cd559f4cc1e
Anton <81695983+LolfaceToxa@users.noreply.github.com>
2021-10-23 15:09:59 +0300

    Update jpeg.py

--- jpeg.py
+++ jpeg.py
@@ -2,7 +2,6 @@ from pdf2image import convert_from_path
 from os import mkdir
 
 
-# например "Qj3z6UUuCic.pdf", "jpeg_files"
 def pdf_to_jpeg(filename, save):
     pages = convert_from_path(filename, 500, poppler_path=r'C:\Program Files\poppler-0.68.0\bin')
     try:

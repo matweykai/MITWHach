b59c3102065af334068e700e1d87e8976e6f8346
Matwey Ivanov <matveykai@list.ru>
2021-10-24 10:04:55 +0300

    Created new branch for django experementing

--- novus/migrate/views.py
+++ novus/migrate/views.py
@@ -1,15 +1,13 @@
-from django.http import Http404
 from django.shortcuts import render
 
-from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
 import os
 import secrets
-
-from requests import request
-
 # ...
+import novus.pdf_work
+
+
 def index(request):
 
     # ЗАГРУЗКА ФАЙЛА
@@ -19,14 +17,27 @@ def index(request):
         key = secrets.token_urlsafe(16)
         request.session['key'] = str(key)
 
+
         # Загрузка файла на сервер
         myfile = request.FILES['fidedrop_1']
         fs = FileSystemStorage()
         filename = fs.save(os.path.join(request.session.get('key'), myfile.name), myfile)
         uploaded_file_url = fs.url(filename)
-        
+
+        request.session['file_name'] = myfile.name
+
         # ДЕБАГ
         return render(request, 'migrate/index.html', {
             'uploaded_file_url': uploaded_file_url
         })
     return render(request, 'migrate/index.html')
+
+
+def separate(request):
+    key = request.session.get('key')
+    target_path = os.getcwd().replace("\\", '/', os.getcwd().count("\\")) + f'/media/{key}'
+    file_name = request.session.get("file_name")
+
+    novus.pdf_work.split_file(target_path=target_path, file_name=file_name, split_points_indexes=(1, 3, 5))
+
+    return
